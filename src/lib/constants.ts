1cebd3286f2aefef72656cbcea8eea3c3460f286
Akiz-Ivanov <ivanovicandreja@gmail.com>
2026-08-23 22:48:48 +0200

    chore: add BOT_NAME constant

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,3 +4,5 @@ export const wishlistLimitReachedWithRemoveMessage = () =>
   `Your wishlist is at the ${WISHLIST_LIMIT}-game limit — pick something to remove:`
 
 export const AUTOCOMPLETE_MIN_QUERY_LENGTH = 3
+
+export const BOT_NAME = 'PC Game Deals'
