3bdbe5159b286cd4279faee9f57e3393754c2bb0
Akiz-Ivanov <ivanovicandreja@gmail.com>
2026-09-13 15:05:14 +0200

    fix: replace GOG shop emoji with brand icon

--- src/discord/embeds/shopEmoji.ts
+++ src/discord/embeds/shopEmoji.ts
@@ -2,7 +2,7 @@ import { customEmojiTag } from './discordEmoji'
 
 const SHOP_EMOJI_IDS: Record<string, string> = {
   Steam: '1530578986965405726',
-  GOG: '1530581772327321720',
+  GOG: '1548677994627993652',
   'Humble Store': '1538946695029850313',
   'Epic Game Store': '1530581293744521246',
   Fanatical: '1538944690303406080',
